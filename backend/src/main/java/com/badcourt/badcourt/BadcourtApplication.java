f79abc7c65844cfe88672a49e25cc0feab34b19f
Siva <vsiva6@deloitte.com>
2024-11-12 19:02:37 +0530

    inital-setup

---
+++ backend/src/main/java/com/badcourt/badcourt/BadcourtApplication.java
@@ -0,0 +1,13 @@
+package com.badcourt.badcourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadcourtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadcourtApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/badcourt/badcourt/BadcourtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badcourt.badcourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadcourtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
